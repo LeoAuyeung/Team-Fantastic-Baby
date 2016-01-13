eb5484e38d10be229a6a60b95b530e4e6a8ab863
BursTheGravity <yauyeung@stuy.edu>
2016-01-13 01:53:07 -0500

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -2,40 +2,40 @@
 //Tile.java
 
 public class Tile {
-
+	
     private String image;
     private boolean _isPlayerHere;
-
+	
     //constructor 
     public Tile() {
-	image = " ";
-	_isPlayerHere = false;
-    }
-
+		image = " ";
+		_isPlayerHere = false;
+	}
+	
     //accessor
     public boolean isPlayerHere() {
-	return _isPlayerHere;
-    }
-
+		return _isPlayerHere;
+	}
+	
     //mutators
     //switches _isPlayerHere
     public void setIsPlayerHere() {
-	_isPlayerHere = !_isPlayerHere;
-    }
-
+		_isPlayerHere = !_isPlayerHere;
+	}
+	
     //changes the image according to _isPlayerHere
     public void setImage() {
-	if( isPlayerHere() ) {
-	    image = "*";
+		if( isPlayerHere() ) {
+			image = "*";
+		}
+		else {
+			image = " ";
+		}
 	}
-	else {
-	    image = " ";
-	}
-    }
-
-    //overwritten toString that would display the image
+	
+    //overwritten toString that will display the image
     public String toString() {
-	return image;
-    }
-
+		return image;
+	}
+	
 }//ends Tile.java
