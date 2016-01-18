2802dd9e007d98092cc696ed4c6d0484ef514ab0
SungbinK <skim15@stuy.edu>
2016-01-18 14:32:04 -0800

    BATTLE DISPLAY COMPLETE

--- Game.java
+++ Game.java
@@ -128,75 +128,128 @@ public class Game {
 	public void displayBattlefield() {
 		//displays user's pkmn + enemy pkmn on top (half the screen total ?)
 		Battle.reset();
-		Battle.set( 0, 0, "============================================================" );
-		Battle.set( 1, 0, "Lv." + enemyPokemon.getLevel() + "     " + enemyPokemon.getName() + "(" + enemyPokemon.getType() + ")" );
+		Battle.set( 0, 0, "=============================================================" );
+		Battle.set( 1, 0, " Lv." + enemyPokemon.getLevel() + "     " + enemyPokemon.getName() + "(" + enemyPokemon.getType() + ")" );
 		Battle.set( 2, 0, displayHP( enemyPokemon ) + " (" + enemyPokemon.getHP() + "/" + enemyPokemon.getMaxHP() + ")" );
 		Battle.set( 3, 0, displayEXP( enemyPokemon ) + " (" + enemyPokemon.getEXP() + "/" + enemyPokemon.getLevelEXP() + ")" );
-		Battle.set( 4, 0, "------------------------------------------------------------" );
-		Battle.set( 5, 0, "Lv." + currentPokemon.getLevel() + "     " + currentPokemon.getName() + "(" + currentPokemon.getType() + ")" );
+		Battle.set( 4, 0, "-------------------------------------------------------------" );
+		Battle.set( 5, 0, " Lv." + currentPokemon.getLevel() + "     " + currentPokemon.getName() + "(" + currentPokemon.getType() + ")" );
 		Battle.set( 6, 0, displayHP( currentPokemon ) + " (" + currentPokemon.getHP() + "/" + currentPokemon.getMaxHP() + ")" );
 		Battle.set( 7, 0, displayEXP( currentPokemon ) + " (" + currentPokemon.getEXP() + "/" + currentPokemon.getLevelEXP() + ")" );
-		Battle.set( 8, 0, "============================================================" );
+		Battle.set( 8, 0, "=============================================================" );
 	}
 	
 	public void displayBattleMenu() {
-		Battle.set( 10, 0, "          WHAT WILL " + Player.getName() + " DO? " );
-		Battle.set( 12, 0, "            -----------      -----------  " );
-		Battle.set( 13, 0, "            | 1:FIGHT |      |  2:BAG  |  " );
-		Battle.set( 14, 0, "            -----------      -----------  " );
-		Battle.set( 16, 0, "            -----------      -----------  " );
-		Battle.set( 17, 0, "            |3:POKEMON|      |  4:RUN  |  " );
-		Battle.set( 18, 0, "            -----------      -----------  " );
+		Battle.set( 10, 0, "                    WHAT WILL " + Player.getName() + " DO?" );
+		Battle.set( 12, 0, "                -----------       -----------  " );
+		Battle.set( 13, 0, "                | 1:FIGHT |       |  2:BAG  |  " );
+		Battle.set( 14, 0, "                -----------       -----------  " );
+		Battle.set( 16, 0, "                -----------       -----------  " );
+		Battle.set( 17, 0, "                |3:POKEMON|       |  4:RUN  |  " );
+		Battle.set( 18, 0, "                -----------       -----------  " );
 		System.out.println( battle );
 	}
 	
 	public void displayFight() {
-		System.out.println("FIGHT MENU");
+	    Battle.set( 9, 0, "                    WHAT WILL " + currentPokemon.getName() + " DO?          B: BACK" );
+	    Battle.set( 11, 0, "      --------------------       --------------------" );
+	    Battle.set( 12, 0, "      " + indent(" (1) " + currentPokemon.getMovesName(0), 20)  );
+	    Battle.set( 13, 0, "      " + indent(" [" + currentPokemon.getType() + "] PP " + currentPokemon.getCurrentPP(0) + "/" + currentPokemon.getMaxPP(0), 20) );
+	    Battle.set( 14, 0, "      --------------------       --------------------" );
+	    Battle.set( 16, 0, "      --------------------       --------------------" ); 
+	    Battle.set( 17, 0, "      " + indent(" (3) " + currentPokemon.getMovesName(2), 20)  );
+	    Battle.set( 18, 0, "      " + indent(" [" + currentPokemon.getType() + "] PP " + currentPokemon.getCurrentPP(2) + "/" + currentPokemon.getMaxPP(2), 20) );
+	    Battle.set( 19, 0, "      --------------------       --------------------" );
+	    Battle.set( 12, 1, "       " + indent(" (2) " + currentPokemon.getMovesName(1), 20)  );
+	    Battle.set( 13, 1, "       " + indent(" [" + currentPokemon.getType() + "] PP " + currentPokemon.getCurrentPP(1) + "/" + currentPokemon.getMaxPP(1), 20) );
+	    Battle.set( 17, 1, "       " + indent(" (4) " + currentPokemon.getMovesName(3), 20)  );
+	    Battle.set( 18, 1, "       " + indent(" [" + currentPokemon.getType() + "] PP " + currentPokemon.getCurrentPP(3) + "/" + currentPokemon.getMaxPP(3), 20) );
+	    System.out.println( battle );	    
 	}
 	
 	public void displayBag() {
-		System.out.println("1: Pokeballs, 2: Potions, 3: Battle Items");
+	    Battle.set( 9, 0, "                      CHOOSE A BAG               B: BACK" );
+	    Battle.set( 11, 0, "                    ----------------" );
+	    Battle.set( 12, 0, "                    |  1:POKEBALLS |" );
+	    Battle.set( 13, 0, "                    ----------------" );
+	    Battle.set( 14, 0, "                    ----------------" );
+	    Battle.set( 15, 0, "                    |  2:POTIONS   |" );
+	    Battle.set( 16, 0, "                    ----------------" );
+	    Battle.set( 17, 0, "                    ----------------" );
+	    Battle.set( 18, 0, "                    |3:BATTLE ITEMS|" );
+	    Battle.set( 19, 0, "                    ----------------" );
+	    System.out.println( battle );
 	}
 	
 	public void displayPokemon() {
-		Battle.set( 9, 0,  "                   AVAILABLE POKEMON:                 B: BACK" );
+		Battle.set( 9, 0,  "                      AVAILABLE POKEMON:              B: BACK" );
 		Battle.set( 10, 0, "------------------------------ ------------------------------" );
-		Battle.set( 11, 0, indent(" (1) Lv." + _Pokemon.get(0).getLevel() + "   " + _Pokemon.get(0).getName() + "(" + _Pokemon.get(0).getType() + ")") );
-		Battle.set( 12, 0, indent(displayHP( _Pokemon.get(0) )) );
+		Battle.set( 11, 0, indent(" (1) Lv." + _Pokemon.get(0).getLevel() + " " + _Pokemon.get(0).getName() + "(" + _Pokemon.get(0).getType() + ")", 30) );
+		Battle.set( 12, 0, indent(displayHP( _Pokemon.get(0) ), 30) );
 		Battle.set( 13, 0, "------------------------------ ------------------------------" );
-		Battle.set( 14, 0, indent(" (2) Lv." + _Pokemon.get(1).getLevel() + "   " + _Pokemon.get(1).getName() + "(" + _Pokemon.get(1).getType() + ")") );
-		Battle.set( 15, 0, indent(displayHP( _Pokemon.get(1) )) );
+		Battle.set( 14, 0, indent(" (2) Lv." + _Pokemon.get(1).getLevel() + " " + _Pokemon.get(1).getName() + "(" + _Pokemon.get(1).getType() + ")", 30) );
+		Battle.set( 15, 0, indent(displayHP( _Pokemon.get(1) ), 30) );
 		Battle.set( 16, 0, "------------------------------ ------------------------------" );
-		Battle.set( 17, 0, indent(" (3) Lv." + _Pokemon.get(2).getLevel() + "   " + _Pokemon.get(2).getName() + "(" + _Pokemon.get(2).getType() + ")") );
-		Battle.set( 18, 0, indent(displayHP( _Pokemon.get(2) )) );
+		Battle.set( 17, 0, indent(" (3) Lv." + _Pokemon.get(2).getLevel() + " " + _Pokemon.get(2).getName() + "(" + _Pokemon.get(2).getType() + ")", 30) );
+		Battle.set( 18, 0, indent(displayHP( _Pokemon.get(2) ), 30) );
 		Battle.set( 19, 0, "------------------------------ ------------------------------" );
 		// second column
-		Battle.set( 11, 1, " " + indent(" (4) Lv." + _Pokemon.get(3).getLevel() + "   " + _Pokemon.get(3).getName() + "(" + _Pokemon.get(3).getType() + ")") );
-		Battle.set( 12, 1, " " + indent(displayHP( _Pokemon.get(3) )) );
-		Battle.set( 14, 1, " " + indent(" (5) Lv." + _Pokemon.get(4).getLevel() + "   " + _Pokemon.get(4).getName() + "(" + _Pokemon.get(4).getType() + ")") );
-		Battle.set( 15, 1, " " + indent(displayHP( _Pokemon.get(4) )) );
-		Battle.set( 17, 1, " " + indent(" (6) Lv." + _Pokemon.get(5).getLevel() + "   " + _Pokemon.get(5).getName() + "(" + _Pokemon.get(5).getType() + ")") );
-		Battle.set( 18, 1, " " + indent(displayHP( _Pokemon.get(5) )) );
+		Battle.set( 11, 1, " " + indent(" (4) Lv." + _Pokemon.get(3).getLevel() + " " + _Pokemon.get(3).getName() + "(" + _Pokemon.get(3).getType() + ")", 30) );
+		Battle.set( 12, 1, " " + indent(displayHP( _Pokemon.get(3) ), 30) );
+		Battle.set( 14, 1, " " + indent(" (5) Lv." + _Pokemon.get(4).getLevel() + " " + _Pokemon.get(4).getName() + "(" + _Pokemon.get(4).getType() + ")", 30) );
+		Battle.set( 15, 1, " " + indent(displayHP( _Pokemon.get(4) ), 30) );
+		Battle.set( 17, 1, " " + indent(" (6) Lv." + _Pokemon.get(5).getLevel() + " " + _Pokemon.get(5).getName() + "(" + _Pokemon.get(5).getType() + ")", 30) );
+		Battle.set( 18, 1, " " + indent(displayHP( _Pokemon.get(5) ), 30) );
 		System.out.println( battle );
 	}
 	
 	public void displayBag1() {
 		//displays user's Pokeballs; 1. Poke Ball 2. Great Ball 3. Ultra Ball 4. Master Ball
+	    Battle.set( 9, 0, "                        POKEBALLS               B: BACK" );
+	    Battle.set( 11, 0,"                  ----------------------" );
+	    Battle.set( 12, 0,"                  " + indent(" 1: Poke Ball  X" + bag.getPokeball(0), 22) );
+	    Battle.set( 13, 0,"                  ----------------------" );
+	    Battle.set( 14, 0,"                  " + indent(" 2: Great Ball  X" + bag.getPokeball(1), 22) );
+	    Battle.set( 15, 0,"                  ----------------------" );
+	    Battle.set( 16, 0,"                  " + indent(" 3: Ultra Ball  X" + bag.getPokeball(2), 22) );
+	    Battle.set( 17, 0,"                  ----------------------" );
+	    Battle.set( 18, 0,"                  " + indent(" 4: Master Ball  X" + bag.getPokeball(3), 22) );
+	    Battle.set( 19, 0,"                  ----------------------" );
+	    System.out.println( battle );
 	}
 	
 	public void displayBag2() {
 		//displays user's Potions; 1. Potion 2. Super Potion 3. Hyper Potion 4. Full Restore 5. Elixir 6. Max Elixir; Elixirs are for PP
+	    Battle.set( 10, 0, "                         POTIONS                 B: BACK" );
+	    Battle.set( 12, 0, "   ------------------------- -------------------------" );
+	    Battle.set( 13, 0, "   " + indent(" 1: Potion  X" + bag.getPotion(0), 25) );
+	    Battle.set( 14, 0, "   ------------------------- -------------------------" );
+	    Battle.set( 15, 0, "   " + indent(" 2: Super Potion  X" + bag.getPotion(1), 25) );	    
+	    Battle.set( 16, 0, "   ------------------------- -------------------------" );
+	    Battle.set( 17, 0, "   " + indent(" 3: Hyper Potion  X" + bag.getPotion(2), 25) );
+	    Battle.set( 18, 0, "   ------------------------- -------------------------" );
+	    Battle.set( 13, 1, " " + indent(" 4: Full Restore  X" + bag.getPotion(3), 25) );
+	    Battle.set( 15, 1, " " + indent(" 5: Elixir Potion  X" + bag.getPotion(4), 25) );
+	    Battle.set( 17, 1, " " + indent(" 6: Max Elixir  X" + bag.getPotion(5), 25) );
+	    System.out.println( battle );
 	}
 	
 	public void displayBag3() {
-		//displays user's Battle Items; 1. Protein 2. Iron 
+		//displays user's Battle Items; 1. Protein 2. Iron
+	    Battle.set( 11, 0, "                       BATTLE ITEMS              B: BACK" );
+	    Battle.set( 13, 0, "                  ----------------------" );
+	    Battle.set( 14, 0, "                  " + indent( " 1: Protein  X" + bag.getBattleItem(0), 22 ) );
+	    Battle.set( 15, 0, "                  ----------------------" );
+	    Battle.set( 16, 0, "                  " + indent( " 2: Iron  X" + bag.getBattleItem(0), 22 ) );
+	    Battle.set( 17, 0, "                  ----------------------" );	    
+	    System.out.println( battle );
 	}
 	
 	//method to turn HP into a bar
 	public String displayHP( Pokemon p ) {
-		double eachBar = ( ( p.getMaxHP() ) / 20 ); // this is how much hp each bar is worth
+		double eachBar = ( ( p.getMaxHP() ) / 20.0 ); // this is how much hp each bar is worth
 		int numBar = (int)( ( p.getHP() ) / eachBar); // number of bars displayed
-		String bar = "HP[";
+		String bar = " HP[";
 		for( int n = 0; n < numBar; n++ ) {
 			bar += "|";
 		}
@@ -223,7 +276,7 @@ public class Game {
 	}
 	
 	// helper method to equally space out the first column of "displayPokemon()"
-	public String indent( String s ) {
+    public String indent( String s, int x ) {
 		String temp = new String( s ); //copy string
 		int numChar = 0;
 		while( temp.length() > 0 ) {
@@ -231,7 +284,7 @@ public class Game {
 			numChar++;
 		}
 		s = "|" + s;
-		for( int n = 0; n < (28 - numChar); n++ ) {
+		for( int n = 0; n < ((x - 2) - numChar); n++ ) {
 			s += " ";
 		}
 		s += "|";
@@ -382,7 +435,7 @@ public class Game {
 			}
 			catch (Exception e) { battleScreen = 3; }
 			if ( selectedPokemon <= _Pokemon.size() && !( _Pokemon.get(selectedPokemon).equals(currentPokemon) ) ) {
-				currentPokemon = _Pokemon.get( selectedPokemon - 1 ); //Swaps out Pokemon
+				currentPokemon = _Pokemon.get( selectedPokemon ); //Swaps out Pokemon
 				battleScreen = 0; //Goes back to default menu
 				opponentTurn = true;
 			}
@@ -486,7 +539,8 @@ public class Game {
 				battleScreen = 0; //Goes back to default menu
 				opponentTurn = true;
 			}
-			if ( command.equals("b") ) { battleScreen = 2; } //Goes back to previous screen
+			if ( command.equals("b") ) { battleScreen = 2;
+			} //Goes back to previous screen
 		}//ends Bag -- Potions
 		
 		//Bag -- Battle Items
@@ -543,7 +597,7 @@ public class Game {
 		if (starter == 1) { captured = new Bulbasaur(); }
 		else if (starter == 2) { captured = new Charmander(); }
 		else if (starter == 3) { captured = new Squirtle(); }
-		_Pokemon.add( captured ); //adds starter to list _Pokemon
+		_Pokemon.set( 0, captured ); //adds starter to list _Pokemon
 		selectedPokemon = 0;
 		currentPokemon = _Pokemon.get(selectedPokemon);
 		
@@ -569,7 +623,7 @@ public class Game {
 			else if ( battleMode == true ) { executeBattleControl( control ); }
 			System.out.println("battlemode: " + battleMode);
 			
-			//if ( battleMode == true ) { displayBattle2(); } //displays battle msg after user does something; implementing later
+			//if ( battleMode == true ) { displayBattle(); } //displays battle msg after user does something; implementing later
 			
 			/*//Checks if Enemy Pokemon has been defeated
 			if ( battleMode == true && enemyPokemon.fainted() ) {
--- Pokedex/Pokemon.java
+++ Pokedex/Pokemon.java
@@ -9,110 +9,115 @@ public abstract class Pokemon {
     protected String name, type;
     protected int HP, maxHP, attack, baseAttack, defense, baseDefense, speed, level, exp, levelEXP;
     protected String[] weaknesses, strengths;
-	protected int movesNum;
-    protected String[] movesName;
-    protected Integer[] movesDmg, currentPP, maxPP;
+    protected int movesNum;
+    protected String[] movesName = {"A","B","C","D"}; // FOR TESTING 
+    protected Integer[] movesDmg;
+    protected Integer[] currentPP = {20,30,40,8};
+    protected Integer[] maxPP = {20,30,40,8};
 	
     //Constructor
     public Pokemon() {
-		name = "None";
-		maxHP = 0;
-		HP = 0;
-		level = 0;
-		type = "None";
-	}
+	name = "None";
+	maxHP = 0;
+	HP = 0;
+	level = 0;
+	type = "None";
+    }
 	
     public Pokemon ( String n, int hp, int atk, int def, int spd, int lvl, int e, String t ) {
-		name = n;
-		maxHP = hp;
-		HP = maxHP;
-		attack = atk;
-		baseAttack = atk;
-		defense = def;
-		baseDefense = def;
-		speed = spd;
-		level = lvl;
-		exp = 0;
-		levelEXP = e;
-		type = t;
-	}
+	name = n;
+	maxHP = hp;
+	HP = maxHP;
+	attack = atk;
+	baseAttack = atk;
+	defense = def;
+	baseDefense = def;
+	speed = spd;
+	level = lvl;
+	exp = 0;
+	levelEXP = e;
+	type = t;
+    }
 	
     //Mutators
     public void setWild ( boolean w ) { wild = w; }
     public void setName ( String n ) { name = n; }
     public void setType ( String t ) { type = t; }
-	public void setHP ( int hp ) { HP = hp; }
-	public void setMaxHP ( int hp ) { maxHP = hp; }
-	public void setAttack ( int atk ) { attack = atk; }
-	public void setDefense ( int def ) { defense = def; }
-	public void setSpeed ( int spd ) { speed = spd; }
-	public void setLevel ( int lvl ) { level = lvl; }
+    public void setHP ( int hp ) { HP = hp; }
+    public void setMaxHP ( int hp ) { maxHP = hp; }
+    public void setAttack ( int atk ) { attack = atk; }
+    public void setDefense ( int def ) { defense = def; }
+    public void setSpeed ( int spd ) { speed = spd; }
+    public void setLevel ( int lvl ) { level = lvl; }
     public void setLevelEXP ( int lvlxp ) { levelEXP = lvlxp; }
     public void setEXP ( int xp ) { exp = xp; }
-	public void setMoves ( int mvs ) { movesNum = mvs; }
+    public void setMoves ( int mvs ) { movesNum = mvs; }
 	
-	public boolean fainted() { return HP > 0; }
+    public boolean fainted() { return HP > 0; }
 	
-	//Accessors
-	public boolean getWild() { return wild; }
-	public String getName() { return name; }
-	public String getType() { return type; }
-	public int getHP() { return HP; }
-	public int getMaxHP() { return maxHP; }
-	public int getAttack() { return attack; }
-	public int getDefense() { return defense; }
-	public int getSpeed() { return speed; }
-	public int getLevel() { return level; }
+    //Accessors
+    public boolean getWild() { return wild; }
+    public String getName() { return name; }
+    public String getType() { return type; }
+    public int getHP() { return HP; }
+    public int getMaxHP() { return maxHP; }
+    public int getAttack() { return attack; }
+    public int getDefense() { return defense; }
+    public int getSpeed() { return speed; }
+    public int getLevel() { return level; }
     public int getLevelEXP() { return levelEXP; }
     public int getEXP() { return exp; }
-	public int getMovesNum() { return movesNum; }
+    public int getMovesNum() { return movesNum; }
+    public String getMovesName( int n ) { return movesName[n]; }
+    public int getCurrentPP( int n ) { return currentPP[n]; }
+    public int getMaxPP( int n ) { return maxPP[n]; }
 	
-	//Battle methods
+    //Battle methods
 	public int attack (Pokemon enemy, int n) {
-		int move = n - 1;
-		double effectiveness = 1;
-		/*if ( weaknesses.contains( enemy.getType() ) ) { effectiveness = 0.5; }
-		else if ( strengths.contains( enemy.getType() ) ) { effectiveness = 2; }*/
-		int baseDmg = movesDmg[move];
-		int damageDealt = (int) ( ( ( (2 * getLevel() + 10) / 250 ) * ( getAttack() / enemy.getDefense() ) * baseDmg + 2 ) * effectiveness );
-		//actual formula for dmg in Pokemon; http://bulbapedia.bulbagarden.net/wiki/Damage#Damage_formula
-		enemy.lowerHP(damageDealt);
-		currentPP[move] -= 1;
+	int move = n - 1;
+	double effectiveness = 1;
+	/*if ( weaknesses.contains( enemy.getType() ) ) { effectiveness = 0.5; }
+	  else if ( strengths.contains( enemy.getType() ) ) { effectiveness = 2; }*/
+	int baseDmg = movesDmg[move];
+	int damageDealt = (int) ( ( ( (2 * getLevel() + 10) / 250 ) * ( getAttack() / enemy.getDefense() ) * baseDmg + 2 ) * effectiveness );
+	//actual formula for dmg in Pokemon; http://bulbapedia.bulbagarden.net/wiki/Damage#Damage_formula
+	enemy.lowerHP(damageDealt);
+	currentPP[move] -= 1;
 		
-		return damageDealt;
-	}
+	return damageDealt;
+    }
 	
-	public void normalize() {
-		attack = baseAttack;
-		defense = baseDefense;
-	}
+    public void normalize() {
+	attack = baseAttack;
+	defense = baseDefense;
+    }
 	
-	public void restoreHP( int amount ) {
-		setHP ( HP + amount );
-		if ( HP > maxHP ) { HP = maxHP; }
-	}
+    public void restoreHP( int amount ) {
+	setHP ( HP + amount );
+	if ( HP > maxHP ) { HP = maxHP; }
+    }
 	
-	public void lowerHP( int amount ) {
-		setHP ( HP - amount );
-		if ( HP < 0 ) { HP = 0; }
-	}
+    public void lowerHP( int amount ) {
+	setHP ( HP - amount );
+	if ( HP < 0 ) { HP = 0; }
+    }
 	
-	public void restorePP( int amount ) {
-		for ( int i = 0; i < movesNum; i++ ) {
-			currentPP[i] = currentPP[i] + amount;
-			if ( currentPP[i] > maxPP[i] ) { currentPP[i] = maxPP[i]; }
-		}
+    public void restorePP( int amount ) {
+	for ( int i = 0; i < movesNum; i++ ) {
+	    currentPP[i] = currentPP[i] + amount;
+	    if ( currentPP[i] > maxPP[i] ) { currentPP[i] = maxPP[i]; }
 	}
+    }
 	
-	public boolean allMaxPP() {
-		for ( int i = 0; i < movesNum; i++ ) {
-			if ( currentPP[i] < maxPP[i] ) { return false; }
-		}
-		return true;
+    public boolean allMaxPP() {
+	for ( int i = 0; i < movesNum; i++ ) {
+	    if ( currentPP[i] < maxPP[i] ) { return false; }
 	}
+	return true;
+    }
 	
-	//description of Pokemon
-	public abstract String about();
+    //description of Pokemon
+    public abstract String about();
 	
 }
 
