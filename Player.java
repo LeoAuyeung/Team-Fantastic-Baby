577dfd320aa945206c0c7dbbf7887699ae1daece
SungbinK <skim15@stuy.edu>
2016-01-25 04:26:49 -0800

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,57 @@
+//Team Fantastic Baby -- Leo Au-Yeung, Sungbin Kim
+//Player.java
+
+public class Player {
+	
+    private static String gender, name;
+    private static int badges, money, mapNum, fromMap, xCor, yCor, pokemonLeft, RxCor, RyCor, Rmap;
+    private static String direction;
+	
+    //Constructor
+    public Player () {
+
+		badges = 0;
+		money = 5000;
+		//Default map location
+		mapNum = 1;
+		fromMap = mapNum;
+		direction = "RIGHT";
+		xCor = 1;
+		yCor = 3;
+		Rmap = 2;
+		RxCor = 11;  // Respawn location
+		RyCor = 14;
+	}		
+	
+    //Mutators
+    public static void setGender( String g ) { gender = g; }
+    public static void setName( String n ) { name = n; }
+    public static void setX ( int x ) { xCor = x; }
+    public static void setY ( int y ) { yCor = y; }
+    public static void setRmap( int n ) { Rmap = n; }
+    public static void setRX ( int x ) { RxCor = x; }
+    public static void setRY ( int y ) { RyCor = y; }   
+    public static void setMoney ( int m ) { money = m; }
+    public static void setPokemonLeft ( int l ) { pokemonLeft = l; }
+    public static void setBadges ( int b ) { badges = b; }
+    public static void setDirection ( String d ) { direction = d; }
+    public static void setMapNum ( int n ) { mapNum = n; }
+    public static void setFromMap ( int n ) { fromMap = n; }
+    public static boolean blackedOut() { return pokemonLeft <= 0; }
+	
+    //Accessors
+    public static String getGender() { return gender; }
+    public static String getName() { return name; }
+    public static int getX() { return xCor; }
+    public static int getY() { return yCor; }
+    public static int getRmap() { return Rmap; }
+    public static int getRX() { return RxCor; }
+    public static int getRY() { return RyCor; } 
+    public static int getMoney() { return money; }
+    public static int getPokemonLeft() { return pokemonLeft; }
+    public static int getBadges() { return badges; }
+    public static int getMapNum() { return mapNum; }
+    public static int getFromMap() { return fromMap; }
+    public static String getDirection() { return direction; }
+	
+}
